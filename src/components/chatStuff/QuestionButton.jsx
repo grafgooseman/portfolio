1f4f833e3352d7a03519755997ad415f3f28d6d9
artem <56235052+grafgooseman@users.noreply.github.com>
2022-12-28 20:43:03 -0500

    Panels buttons now becomes blue if pressed

--- src/components/chatStuff/QuestionButton.jsx
+++ src/components/chatStuff/QuestionButton.jsx
@@ -1,19 +1,62 @@
 import React from 'react';
 import styled from 'styled-components';
 import panelQuestionTail from '../../images/panelQuestionTail.svg';
+import panelQuestionTailBlue from '../../images/panelQuestionTailBlue.svg';
+import '../../styles/rippleAnimation.css';
 
-export default function QuestionButton({ text}) {
+export default function QuestionButton({ text }) {
+	let [ currentTail, setCurrentTail ] = React.useState(panelQuestionTail);
+
+	function handleEvent(event) {
+		//Omg, because I change out the SVGs, the animation transition: all looks terrible, tail is still instant
+		if (event.type === 'mousedown') {
+			setCurrentTail(panelQuestionTailBlue)
+		} else {
+			setCurrentTail(panelQuestionTail)
+		}
+	}
+
+	// function handleClickEvent(event) {
+	// 	console.log(event);
+	// 	setCurrentTail(panelQuestionTailBlue);
+	// }
+
 	return (
 		<Wrapper>
-			<MiddleWrapper>
+			<MiddleWrapper onMouseDown={handleEvent} onMouseUp={handleEvent}>
 				<Bubble>
 					<p>{text}</p>
-					<img src={panelQuestionTail} alt="" />
+					<img src={currentTail} alt="" />
 				</Bubble>
 			</MiddleWrapper>
 		</Wrapper>
 	);
 }
+
+//Functions
+// function createRipple(event) {
+// 	const button = event.currentTarget;
+
+// 	const circle = document.createElement('span');
+// 	const diameter = Math.max(button.clientWidth, button.clientHeight);
+// 	const radius = diameter / 2;
+
+// 	circle.style.width = circle.style.height = `${diameter}px`;
+// 	circle.style.left = `${event.clientX - button.offsetLeft - radius}px`;
+// 	circle.style.top = `${event.clientY - button.offsetTop - radius}px`;
+// 	circle.classList.add('ripple');
+
+// 	console.table({ button, circle, diameter, radius });
+// 	console.table(circle.style.left, circle.style.top);
+
+// 	const ripple = button.getElementsByClassName('ripple')[0];
+
+// 	if (ripple) {
+// 		ripple.remove();
+// 	}
+
+// 	button.appendChild(circle);
+// }
 
 //Styles
 const BUTTON_COLOR = '#0078FF';
@@ -25,7 +68,7 @@ const Wrapper = styled.div`
 	width: 100%;
 `;
 
-const MiddleWrapper = styled.div`
+const MiddleWrapper = styled.button`
 	margin: 1px 10px 1px 10px;
 	border-radius: 20px;
 	border: 1.3px solid ${BUTTON_COLOR};
@@ -33,6 +76,19 @@ const MiddleWrapper = styled.div`
 	align-items: center;
 	justify-content: center;
 	color: ${BUTTON_COLOR};
+
+	background-color: transparent;
+	padding: 0;
+	font: inherit;
+	cursor: pointer;
+	outline: inherit;
+
+	:active {
+		background-color: ${BUTTON_COLOR};
+		color: white;
+	}
+	/* transition: all 100ms; */
+	/* transition: background 400ms; */
 
 	-webkit-user-select: none; /* Safari */
 	-moz-user-select: none; /* Firefox */
@@ -59,12 +115,20 @@ const Bubble = styled.div`
 	/* background: #e9e9eb; */
 	border-radius: 20px;
 
+	polyline,
+	path {
+		fill: inherit;
+		stroke: inherit;
+	}
+
 	img {
 		position: absolute;
 		width: 16.5px;
 		height: 17px;
 		right: -18.5px;
 		bottom: -4px;
+
+		fill: ${BUTTON_COLOR};
 	}
 
 	p {
