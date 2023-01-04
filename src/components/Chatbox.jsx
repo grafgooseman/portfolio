ac2a782b5b4f32aed8a2467a6e18de786e679df2
artem <56235052+grafgooseman@users.noreply.github.com>
2023-01-04 16:30:25 -0500

    Chat scrolls, correct delays implemented

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -14,7 +14,9 @@ const replyDelay = 1700;
 const questionDelay = 800;
 
 export default function Chatbox() {
+	const ChatboxRef = useRef();
 	const [ returnArray, setReturnArray ] = useState([]);
+	const returnArrayCopy = [];
 	const mappingOfTopics = getTriggerButtonTextMappings();
 	const usedMappings = [];
 	let isAwaitsReply = false;
@@ -25,87 +27,206 @@ export default function Chatbox() {
 		}
 	}, []);
 
+	// useEffect(
+	// 	() => {
+	// 		console.log('useEffect: ');
+	// 		console.log(returnArray);
+	// 		returnArrayCopy.push([ ...returnArray ]);
+	// 	},
+	// 	[ returnArray ]
+	// );
+
 	//#region JSX functions
 
-	function renderQuestionButtons(dontSetArray) {
-		const localArr = [];
+	async function renderElement(element, delay) {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				setReturnArray((returnArray) => [ ...returnArray, element ]);
+				const chatbox = ChatboxRef.current;
+				const paddingTop = parseInt(getComputedStyle(chatbox).paddingTop, 10);
+				const paddingBottom = parseInt(getComputedStyle(chatbox).paddingBottom, 10);
+				const scrollbarHeight = chatbox.offsetHeight - chatbox.clientHeight;
+				const targetPosition =
+					chatbox.scrollHeight - chatbox.clientHeight - scrollbarHeight + paddingTop + paddingBottom;
 
+				// TODO: scroll to bottom including padding (current implementation doesnt scroll all the way to the bottom)
+				
+				chatbox.scrollTo({
+					top: targetPosition,
+					behavior: 'smooth'
+				});
+
+				resolve();
+			}, delay);
+		});
+	}
+
+	async function renderQuestionButtons() {
 		if (usedMappings.length === 0) {
-			localArr.push(
-				<QuestionButton text={mappingOfTopics.greeting} callBack={questionButtonPressed} key={'btn-' + uid()} />
+			await renderElement(
+				<QuestionButton
+					text={mappingOfTopics.greeting}
+					callBack={questionButtonPressed}
+					key={'btn-' + uid()}
+				/>,
+				0
 			);
 		} else {
+			let firstElement = true;
 			for (let i = 1; i < Object.keys(mappingOfTopics).length; i++) {
 				const key = Object.keys(mappingOfTopics)[i];
 				if (mappingOfTopics.hasOwnProperty(key)) {
 					const element = mappingOfTopics[key];
+
 					if (!usedMappings.includes(element)) {
-						localArr.push(
-							<QuestionButton key={'btn-' + uid()} callBack={questionButtonPressed} text={element} />
+						let delay = 0;
+						if (firstElement) {
+							delay = replyDelay + 100;
+							firstElement = false;
+						} else {
+							delay = 0;
+						}
+
+						await renderElement(
+							<QuestionButton text={element} callBack={questionButtonPressed} key={'btn-' + uid()} />,
+							delay
 						);
 					}
 				}
 			}
 		}
-		if (!dontSetArray) {
-			setReturnArray([ ...returnArray, ...localArr ]);
-		}
-		console.log(returnArray);
 		isAwaitsReply = true;
-		return localArr;
 	}
 
-	async function questionButtonPressed(btnText) {
+	function questionButtonPressed(btnText) {
 		const msgSection = Object.entries(mappingOfTopics).find((entry) => entry[1] === btnText)[0];
 		usedMappings.push(btnText);
-
-		const localArr = [];
-		localArr.push(await renderQuestionAnswerBlock(getMessageBlock(msgSection)));
-		setReturnArray([ ...returnArray, ...localArr ]);
-		
 		pergeOldButtons();
+		renderQuestionAnswerBlock(getMessageBlock(msgSection));
 	}
 
 	async function renderQuestionAnswerBlock(messagesArray) {
-		// const localArr = [];
-
 		let previousType = 'question';
+		let firstElement = true;
 		for (let i = 0; i < messagesArray.length; i++) {
 			if (messagesArray[i].type !== previousType) {
-				returnArray.push(<Spacer key={uid()} />);
+				await renderElement(<Spacer key={uid()} />, 0);
 			}
 			if (messagesArray[i].type === 'reply') {
-				returnArray.push(
-					<ReplyBubble key={'message-' + uid()} paddingArray={paddingArray} text={messagesArray[i].text} />
+				await renderElement(
+					<ReplyBubble key={'message-' + uid()} paddingArray={paddingArray} text={messagesArray[i].text} />,
+					replyDelay
 				);
 				previousType = 'reply';
-				await delayedElementRender(replyDelay);
 			} else if (messagesArray[i].type === 'question') {
-				returnArray.push(
-					<QuestionBubble key={'message-' + uid()} paddingArray={paddingArray} text={messagesArray[i].text} />
+				let delay = 0;
+				if (firstElement) {
+					delay = 0;
+					firstElement = false;
+				} else {
+					delay = questionDelay;
+				}
+
+				await renderElement(
+					<QuestionBubble
+						key={'message-' + uid()}
+						paddingArray={paddingArray}
+						text={messagesArray[i].text}
+					/>,
+					delay
 				);
 				previousType = 'question';
-				await delayedElementRender(questionDelay);
 			}
 		}
-		returnArray.push(<Spacer key={uid()} />);
-		setReturnArray(returnArray);
-		// setReturnArray([...returnArray, ...localArr]); //why this doesnt work idk
+		renderQuestionButtons();
 	}
 
 	function pergeOldButtons() {
-		const filteredArray = returnArray.filter((element) => element && element.key.includes('btn-') === false);
-		setReturnArray([ ...filteredArray, renderQuestionButtons() ]);
+		console.log('pergeOldButtons');
+		console.log(returnArray);
+		// const filteredArray = returnArray.filter((element) => element && element.key.includes('btn-') === false);
+		// setReturnArray(returnArray => [ ...filteredArray, renderQuestionButtons() ]);
 	}
 
-	async function delayedElementRender(delay) {
-		await new Promise((resolve) => setTimeout(resolve, delay));
-		setReturnArray([...returnArray]);
-	}
+	// function renderQuestionButtons(dontSetArray) {
+	// 	const localArr = [];
+
+	// 	if (usedMappings.length === 0) {
+	// 		localArr.push(
+	// 			<QuestionButton text={mappingOfTopics.greeting} callBack={questionButtonPressed} key={'btn-' + uid()} />
+	// 		);
+	// 	} else {
+	// 		for (let i = 1; i < Object.keys(mappingOfTopics).length; i++) {
+	// 			const key = Object.keys(mappingOfTopics)[i];
+	// 			if (mappingOfTopics.hasOwnProperty(key)) {
+	// 				const element = mappingOfTopics[key];
+	// 				if (!usedMappings.includes(element)) {
+	// 					localArr.push(
+	// 						<QuestionButton key={'btn-' + uid()} callBack={questionButtonPressed} text={element} />
+	// 					);
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// 	if (!dontSetArray) {
+	// 		setReturnArray([ ...returnArray, ...localArr ]);
+	// 	}
+	// 	console.log(returnArray);
+	// 	isAwaitsReply = true;
+	// 	return localArr;
+	// }
+
+	// async function questionButtonPressed(btnText) {
+	// 	const msgSection = Object.entries(mappingOfTopics).find((entry) => entry[1] === btnText)[0];
+	// 	usedMappings.push(btnText);
+
+	// 	const localArr = [];
+	// 	localArr.push(await renderQuestionAnswerBlock(getMessageBlock(msgSection)));
+	// 	setReturnArray([ ...returnArray, ...localArr ]);
+
+	// 	pergeOldButtons();
+	// }
+
+	// async function renderQuestionAnswerBlock(messagesArray) {
+	// 	// const localArr = [];
+
+	// 	let previousType = 'question';
+	// 	for (let i = 0; i < messagesArray.length; i++) {
+	// 		if (messagesArray[i].type !== previousType) {
+	// 			returnArray.push(<Spacer key={uid()} />);
+	// 		}
+	// 		if (messagesArray[i].type === 'reply') {
+	// 			returnArray.push(
+	// 				<ReplyBubble key={'message-' + uid()} paddingArray={paddingArray} text={messagesArray[i].text} />
+	// 			);
+	// 			previousType = 'reply';
+	// 			await delayedElementRender(replyDelay);
+	// 		} else if (messagesArray[i].type === 'question') {
+	// 			returnArray.push(
+	// 				<QuestionBubble key={'message-' + uid()} paddingArray={paddingArray} text={messagesArray[i].text} />
+	// 			);
+	// 			previousType = 'question';
+	// 			await delayedElementRender(questionDelay);
+	// 		}
+	// 	}
+	// 	returnArray.push(<Spacer key={uid()} />);
+	// 	setReturnArray(returnArray);
+	// 	// setReturnArray([...returnArray, ...localArr]); //why this doesnt work idk
+	// }
+
+	// function pergeOldButtons() {
+	// 	const filteredArray = returnArray.filter((element) => element && element.key.includes('btn-') === false);
+	// 	setReturnArray([ ...filteredArray, renderQuestionButtons() ]);
+	// }
+
+	// async function delayedElementRender(delay) {
+	// 	await new Promise((resolve) => setTimeout(resolve, delay));
+	// 	setReturnArray([...returnArray]);
+	// }
 
 	//#endregion
 
-	return <Wrapper>{returnArray}</Wrapper>;
+	return <Wrapper ref={ChatboxRef}>{returnArray}</Wrapper>;
 }
 
 //#region CSS Functions
