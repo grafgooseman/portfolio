76f25ac4e2962a3ac4e6cd934f88a03b356821f7
artem <56235052+grafgooseman@users.noreply.github.com>
2023-01-04 22:48:02 -0500

    Chatbox operational

--- src/components/Chatbox.jsx
+++ src/components/Chatbox.jsx
@@ -16,32 +16,36 @@ const questionDelay = 800;
 export default function Chatbox() {
 	const ChatboxRef = useRef();
 	const [ returnArray, setReturnArray ] = useState([]);
-	const returnArrayCopy = [];
 	const mappingOfTopics = getTriggerButtonTextMappings();
 	const usedMappings = [];
 	let isAwaitsReply = false;
+	let isInitialRender = true;
+
+	const returnArrayCopyRef = useRef(null);
 
 	useEffect(() => {
-		if (isAwaitsReply === false) {
+		if (isAwaitsReply === false && isInitialRender) {
 			renderQuestionButtons();
 		}
 	}, []);
 
-	// useEffect(
-	// 	() => {
-	// 		console.log('useEffect: ');
-	// 		console.log(returnArray);
-	// 		returnArrayCopy.push([ ...returnArray ]);
-	// 	},
-	// 	[ returnArray ]
-	// );
+	useEffect(
+		() => {
+			returnArrayCopyRef.current = returnArray;
+		},
+		[ returnArray ]
+	);
 
 	//#region JSX functions
 
 	async function renderElement(element, delay) {
+		// returnArrayCopy.push(element)
+		// returnArrayCopy = returnArray.slice();
 		return new Promise((resolve) => {
 			setTimeout(() => {
 				setReturnArray((returnArray) => [ ...returnArray, element ]);
+				console.log('---returnArray---');
+				console.log(returnArray);
 				const chatbox = ChatboxRef.current;
 				const paddingTop = parseInt(getComputedStyle(chatbox).paddingTop, 10);
 				const paddingBottom = parseInt(getComputedStyle(chatbox).paddingBottom, 10);
@@ -63,6 +67,7 @@ export default function Chatbox() {
 
 	async function renderQuestionButtons() {
 		if (usedMappings.length === 0) {
+			isInitialRender = false;
 			await renderElement(
 				<QuestionButton
 					text={mappingOfTopics.greeting}
@@ -101,9 +106,14 @@ export default function Chatbox() {
 	function questionButtonPressed(btnText) {
 		const msgSection = Object.entries(mappingOfTopics).find((entry) => entry[1] === btnText)[0];
 		usedMappings.push(btnText);
+
+		//TODO: Add new elemnt to copyRetunArray
+		// returnArrayCopy = returnArray.slice();
+
 		pergeOldButtons();
 		renderQuestionAnswerBlock(getMessageBlock(msgSection));
 	}
+
 
 	async function renderQuestionAnswerBlock(messagesArray) {
 		let previousType = 'question';
@@ -142,87 +152,14 @@ export default function Chatbox() {
 	}
 
 	function pergeOldButtons() {
-		console.log('pergeOldButtons');
-		console.log(returnArray);
-		// const filteredArray = returnArray.filter((element) => element && element.key.includes('btn-') === false);
-		// setReturnArray(returnArray => [ ...filteredArray, renderQuestionButtons() ]);
+		console.log('purgeOldButtons');
+		// console.log(returnArrayCopy);
+		console.log(returnArrayCopyRef.current);
+		console.log(returnArrayCopyRef.current.map((element) => element.key.includes('btn-')));
+		const filteredArray = returnArrayCopyRef.current.filter((element) => element && element.key.includes('btn-') === false);
+		console.log(filteredArray);
+		setReturnArray([ ...filteredArray]);
 	}
-
-	// function renderQuestionButtons(dontSetArray) {
-	// 	const localArr = [];
-
-	// 	if (usedMappings.length === 0) {
-	// 		localArr.push(
-	// 			<QuestionButton text={mappingOfTopics.greeting} callBack={questionButtonPressed} key={'btn-' + uid()} />
-	// 		);
-	// 	} else {
-	// 		for (let i = 1; i < Object.keys(mappingOfTopics).length; i++) {
-	// 			const key = Object.keys(mappingOfTopics)[i];
-	// 			if (mappingOfTopics.hasOwnProperty(key)) {
-	// 				const element = mappingOfTopics[key];
-	// 				if (!usedMappings.includes(element)) {
-	// 					localArr.push(
-	// 						<QuestionButton key={'btn-' + uid()} callBack={questionButtonPressed} text={element} />
-	// 					);
-	// 				}
-	// 			}
-	// 		}
-	// 	}
-	// 	if (!dontSetArray) {
-	// 		setReturnArray([ ...returnArray, ...localArr ]);
-	// 	}
-	// 	console.log(returnArray);
-	// 	isAwaitsReply = true;
-	// 	return localArr;
-	// }
-
-	// async function questionButtonPressed(btnText) {
-	// 	const msgSection = Object.entries(mappingOfTopics).find((entry) => entry[1] === btnText)[0];
-	// 	usedMappings.push(btnText);
-
-	// 	const localArr = [];
-	// 	localArr.push(await renderQuestionAnswerBlock(getMessageBlock(msgSection)));
-	// 	setReturnArray([ ...returnArray, ...localArr ]);
-
-	// 	pergeOldButtons();
-	// }
-
-	// async function renderQuestionAnswerBlock(messagesArray) {
-	// 	// const localArr = [];
-
-	// 	let previousType = 'question';
-	// 	for (let i = 0; i < messagesArray.length; i++) {
-	// 		if (messagesArray[i].type !== previousType) {
-	// 			returnArray.push(<Spacer key={uid()} />);
-	// 		}
-	// 		if (messagesArray[i].type === 'reply') {
-	// 			returnArray.push(
-	// 				<ReplyBubble key={'message-' + uid()} paddingArray={paddingArray} text={messagesArray[i].text} />
-	// 			);
-	// 			previousType = 'reply';
-	// 			await delayedElementRender(replyDelay);
-	// 		} else if (messagesArray[i].type === 'question') {
-	// 			returnArray.push(
-	// 				<QuestionBubble key={'message-' + uid()} paddingArray={paddingArray} text={messagesArray[i].text} />
-	// 			);
-	// 			previousType = 'question';
-	// 			await delayedElementRender(questionDelay);
-	// 		}
-	// 	}
-	// 	returnArray.push(<Spacer key={uid()} />);
-	// 	setReturnArray(returnArray);
-	// 	// setReturnArray([...returnArray, ...localArr]); //why this doesnt work idk
-	// }
-
-	// function pergeOldButtons() {
-	// 	const filteredArray = returnArray.filter((element) => element && element.key.includes('btn-') === false);
-	// 	setReturnArray([ ...filteredArray, renderQuestionButtons() ]);
-	// }
-
-	// async function delayedElementRender(delay) {
-	// 	await new Promise((resolve) => setTimeout(resolve, delay));
-	// 	setReturnArray([...returnArray]);
-	// }
 
 	//#endregion
 
